929dfb7e5c46e03aee07184a59ef3d83d1f840a3
Isaac Idibia <idibiaisaac3@gmail.com>
2022-10-13 22:12:25 +0100

    chat feed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { ChatEngine } from 'react-chat-engine';
+import ChatFeed from './components/ChatFeed';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <p>Chat App</p>
-    </div>
+    <ChatEngine
+      height='100vh'
+      projectID='6fd0d538-00cf-4d1f-a2a3-2f9c49f0da20'
+      userName='Zeek'
+      userSecret='Zeek-Tech1'
+      renderChatFeed={(chatAppProps) => <ChatFeed { ...chatAppProps } /> }
+    />
   );
 }
 
---
+++ src/components/ChatFeed.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const ChatFeed = (props) => {
+  const { chats, activeChat, userName, messages } = props;
+
+  const chat = chats && chats[activeChat];
+
+  const renderMessages = () => {
+    const keys = Object.keys(messages);
+    return keys.map((key, index) => {
+        const message = messages[key];
+        const lastMessageKey = index === 0 ? null : keys[index - 1];
+        const isMyMessage = userName === message.sender.username;
+    })
+  };
+
+  renderMessages();
+
+  return <div>ChatFeed</div>;
+};
+
+export default ChatFeed;
